c39974700e00fe29006ca921b724e88292a53a72
Pavan Kumar Aryasomayajulu <pavan.aryasomayajulu@gmail.com>
2020-08-20 12:05:22 +0530

    Create InserData query.sql

---
+++ Day 1/InserData query.sql
@@ -0,0 +1,22 @@
+Create Table Employees
+(
+     Id int primary key,
+     Name nvarchar(50),
+     Gender nvarchar(10),
+     Salary int
+)
+Go
+
+Insert Into Employees Values (1, 'Mark', 'Male', 5000)
+Insert Into Employees Values (2, 'John', 'Male', 4500)
+Insert Into Employees Values (3, 'Pavan', 'Male', 5000)
+Insert Into Employees Values (4, 'Pam', 'Female', 5500)
+Insert Into Employees Values (5, 'Sara', 'Female', 4000)
+Insert Into Employees Values (6, 'Aradhya', 'Female', 3500)
+Insert Into Employees Values (7, 'Tom', 'Male', 5500)
+Insert Into Employees Values (8, 'Mary', 'Female', 5000)
+Insert Into Employees Values (9, 'Ben', 'Male', 6500)
+Insert Into Employees Values (10, 'Jodi', 'Female', 7000)
+Insert Into Employees Values (11, 'Tom', 'Male', 5500)
+Insert Into Employees Values (12, 'Ron', 'Male', 5000)
+Go
